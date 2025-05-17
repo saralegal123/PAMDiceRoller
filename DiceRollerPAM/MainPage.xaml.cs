fa165dc67ac55b2a1df94d8568cea0b2f8d732c7
Sara Marcely <saradomacario@gmail.com>
2025-05-16 22:25:52 -0300

    PAM - DiceRoller // Criação: Dice, Contador e inicio da inetrface

--- DiceRollerPAM/MainPage.xaml.cs
+++ DiceRollerPAM/MainPage.xaml.cs
@@ -2,24 +2,36 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        Dice dado = new Dice();
+        Contador contador = new Contador();
         public MainPage()
         {
             InitializeComponent();
+            dicePicker.SelectedIndexChanged += DicePicker_SelectedIndexChanged;
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void DicePicker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            if (dicePicker.SelectedItem == null) return;
+            string tipo = dicePicker.SelectedItem.ToString();
+            int faces = int.Parse(tipo.Substring(1)); // Pega o número do tipo (ex: D6 -> 6)
+            dado.QuantidadeDeFaces = faces;
+            numeroEscolhidoPicker.ItemsSource = Enumerable.Range(1, faces).ToList();
+        }
+        private void OnJogarClicked(object sender, EventArgs e)
+        {
+            if (numeroEscolhidoPicker.SelectedItem == null || dicePicker.SelectedItem == null)
+                return;
+            int numeroEscolhido = (int)numeroEscolhidoPicker.SelectedItem;
+            dado.Sortear();
+            contador.Jogar(dado, numeroEscolhido);
+            resultadoLabel.Text = $"Você tirou {dado.FaceParaCima} | Face Opostada: {dado.FaceParaBaixo}";
+            pontosLabel.Text = $"Pontos: {contador.Pontos}";
+            sequenciaLabel.Text = $"Sequência: {contador.Sequencia}";
+            somaLabel.Text = $"Soma das Faces Opostas: {contador.Limite}";
+            if (contador.Venceu == "fim")
+            {
+                resultadoLabel.Text = "Tentativas máximas utilizadas";
+            }
         }
     }
-
 }
