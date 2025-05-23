754da59e4e9e7028b9d63b4279d615278404374b
Sara Marcely <saradomacario@gmail.com>
2025-05-23 19:55:34 -0300

    PAM - DiceRoller //tudo errado

--- DiceRollerPAM/Contador.cs
+++ DiceRollerPAM/Contador.cs
@@ -6,16 +6,38 @@ using System.Threading.Tasks;
 
 namespace DiceRollerPAM
 {
-    internal class Contador
+    using System;
+    namespace DiceRollerPAM
     {
+        internal class Contador
+        {
             public int Sequencia;
             public int Pontos;
             public int Limite;
             public string Venceu;
-
+            public int SomaOposta;
             public void Jogar(Dice DadoJogador, int numeroSelecionado)
             {
                 int n = DadoJogador.FaceParaBaixo;
+                if (DadoJogador.FaceParaCima == numeroSelecionado)
+                {
+                    Pontos++;
+                    Sequencia++;
+                }
+                else
+                {
+                    Sequencia = 0;
+                }
+                SomaOposta += n;
+                if (SomaOposta >= Limite)
+                {
+                    Venceu = "fim";
+                }
+                else
+                {
+                    Venceu = "";
+                }
             }
+        }
     }
 }
